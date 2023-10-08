aab399ac7f1dbbf7a89dd446c68f505416644c80
Tannu Singh <tannusingh17.8.2001@gmail.com>
2023-10-08 09:10:13 +0530

    files added

---
+++ app/src/main/java/com/example/expensemanager/DashboardActivity.java
@@ -0,0 +1,152 @@
+package com.example.expensemanager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.expensemanager.databinding.ActivityDashboardBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    ActivityDashboardBinding binding;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth firebaseAuth;
+
+    int sumExpense=0;
+    int sumIncome=0;
+
+    ArrayList<TransactionModel> transactionModelArrayList;
+    TransactionAdapter transactionAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        firebaseFirestore=FirebaseFirestore.getInstance();
+        firebaseAuth=FirebaseAuth.getInstance();
+        transactionModelArrayList=new ArrayList<>();
+
+        binding.historyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.historyRecyclerView.setHasFixedSize(true);
+
+        firebaseAuth.addAuthStateListener(new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser()==null){
+                    startActivity(new Intent(DashboardActivity.this, MainActivity.class));
+                    finish();
+                }
+            }
+        });
+
+        binding.signoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createSignoutDialog();
+                
+            }
+        });
+
+        binding.addFloatingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(DashboardActivity.this,AddTransactionActivity.class));
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        });
+        binding.chartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(DashboardActivity.this,PieChart.class));
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        });
+
+        binding.refreshBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startActivity(new Intent(DashboardActivity.this,DashboardActivity.class));
+                    finish();
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        });
+
+        loadData();
+    }
+
+    private void createSignoutDialog() {
+        AlertDialog.Builder builder= new AlertDialog.Builder(DashboardActivity.this);
+        builder.setTitle("Log Out").setMessage("Are you sure you want to log out?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                firebaseAuth.signOut();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    private void loadData() {
+        firebaseFirestore.collection("Expenses").document(firebaseAuth.getUid()).collection("Notes").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for(DocumentSnapshot ds: task.getResult()){
+                    TransactionModel model=new TransactionModel(ds.getString("ID"),ds.getString("Note"),ds.getString("Amount"),ds.getString("Type"),ds.getString("Date"));
+
+                    int amount= Integer.parseInt(ds.getString("Amount"));
+                    if(ds.getString("Type").equals("Expense")){
+                        sumExpense+=amount;
+                    }
+                    else
+                    {
+                        sumIncome+=amount;
+                    }
+
+                    transactionModelArrayList.add(model);
+
+                }
+                binding.totalExpense.setText(String.valueOf(sumExpense));
+                binding.totalIncome.setText(String.valueOf(sumIncome));
+                binding.totalBalance.setText(String.valueOf(sumIncome-sumExpense));
+                transactionAdapter=new TransactionAdapter(DashboardActivity.this,transactionModelArrayList);
+                binding.historyRecyclerView.setAdapter(transactionAdapter);
+            }
+        });
+    }
+
+
+
+    }
